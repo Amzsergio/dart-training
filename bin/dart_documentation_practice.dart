2832be4d4a3c05baf952d3a70297117027b85f04
Amzsergio <ac.sergioandres@gmail.com>
2024-09-16 11:48:14 -0500

    required names and positional parameters

--- bin/dart_documentation_practice.dart
+++ bin/dart_documentation_practice.dart
@@ -1,5 +1,9 @@
 import 'package:dart_documentation_practice/functions.dart' as functions;
 
 void main() {
-  print('${functions.isEven(2)}!');
+  int add(int a, int b) => a + b;
+  int subtract(int a, int b) => a - b;
+
+  functions.Operation()
+      .performOperation(numberOne: 15, numberTwo: 2, operation: subtract);
 }
--- lib/functions.dart
+++ lib/functions.dart
@@ -1,5 +1,41 @@
+//  Basic Function Creation
 String greetUser(String name) {
   return 'Hello $name';
 }
 
+// Arrow Functions
 bool isEven(int number) => number % 2 == 0;
+
+//3. Named Parameters with Default Values
+
+String printDetails(
+    {required String name, required int age, String city = 'Unknown'}) {
+  return "the person's details are: $name, $age, $city";
+}
+
+// 4. Required Named Parameters
+
+void createAccount({
+  required String userName,
+  required String password,
+  String? email,
+}) {}
+
+//5. Positional and Named Parameters Combination
+
+void orderPizza(
+  String pizzaType, {
+  bool size = false,
+  bool extraCheese = false,
+}) {}
+
+//6. Higher-Order Functions (Passing Functions as Arguments)
+class Operation {
+  void performOperation({
+    required int numberOne,
+    required int numberTwo,
+    required Function operation,
+  }) {
+    print(operation(numberOne, numberTwo));
+  }
+}
